c33ac7547025536c076597536d7495fd8e5d5fb6
Anitria Blue <anitria.blue@gmail.com>
2020-10-10 14:25:50 -0400

    length indexes string notes

---
+++ 10.10.20.js
@@ -0,0 +1,32 @@
+console.log("helloworld")
+console.log("firstname")
+console.log("\n")
+console.log("lastname") 
+let myName="Anitria"
+console.log(`my name is ${myName}`)
+
+let name="Anitria Blue";
+console.log(name[0]);
+console.log(name[8]);
+console.log(`${name[0]}${name[8]}`);
+
+let firstLetter = name[0];
+let secondLetter = name[8];
+console.log(firstLetter + secondLetter);
+
+let sentence = "Anitria Blue"
+
+console.log(sentence.length);
+
+console.log(sentence[11]);
+
+console.log(sentence[length - 1]);
+
+console.log(sentence.charAt[10])
+console.log(sentence.charAt[20])
+
+
+
+
+
+
