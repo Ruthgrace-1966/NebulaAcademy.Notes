5a387499ea9492e51793813e9751617d6412c9d1
Anitria Blue <anitria.blue@gmail.com>
2020-11-08 12:25:46 -0500

    Added more to the code

--- Assignments/hw10.10.20.moreString.js
+++ Assignments/hw10.10.20.moreString.js
@@ -48,10 +48,62 @@
 // console.log(anyString[anyString.length -1]);
 
 // LEVEL 4
-//     Q6: Given the string 'welcome to my website', what is the character at the 7th index? 
-let website = 'welcome to my website';
-console.log(website.charAt(6));         //e
-// chartAt is a method because of the parenthese
-console.log(website.charAt(7));  // Renders a space
-console.log(website[7]);          //Renders a space
-console.log(website[6]);            //Renders an e
+// //     Q6: Given the string 'welcome to my website', what is the character at the 7th index? 
+// let website = 'welcome to my website';
+// console.log(website.charAt(6));         //e
+// // chartAt is a method because of the parenthese
+// console.log(website.charAt(7));  // Renders a space
+// console.log(website[7]);          //Renders a space
+// console.log(website[6]);            //Renders an 
+
+
+// PRACTICE NUMBER ONE 
+
+// LEVEL 0
+//     Q0: Declare a string with the value equal to your name
+//   myQ0: Declare a variable and assign it a string value equal to your name
+// let string = "Anitria";
+// console.log(string);
+let stringy = "Ruthie";
+console.log(stringy);     
+ //Ruthie
+
+
+//Q1: Given the string, 'WE ARE SHOUTING!' log 'we are shouting'
+ let shout = 'WE ARE SHOUTING!';
+ console.log(shout);     //WE ARE SHOUTING!
+
+
+// LEVEL 1
+//     Q2: Declare a string and log its length 
+let friend = 'girlsNightOut';
+console.log(friend.length);     //13
+     // The .length is a property and NOT a method.
+
+
+// //     Q3: Given the string 'Hello World!', log the 6th character.
+let earthling = 'Hello World!';
+console.log(earthling[6]);      //W
+
+// LEVEL 2
+//     Q4: Given the string 'Welcome World?', replace the question mark with an exclamation
+let earth = 'Welcome World?';
+console.log(earth);     //Welcome World?
+console.log(earth.replace("?", "!"));       //Welcome World!
+
+// LEVEL 3
+//     Q5: Given any string, log the first and last characters
+let relations = 'love of my life';
+console.log(relations);     //love of my life
+console.log(relations.length);      //15
+console.log(relations[0]);      //l
+console.log(relations[14]);     //e
+console.log(relations[relations.length -1]);        //e
+
+
+// LEVEL 4
+//    Q6: Given the string 'welcome to my website', what is the character at the 7th index? 
+let javaScript = 'welcome to my website';
+console.log(javaScript);        //welcome to my website
+console.log(javaScript[7]);     //*This space indicated an empty space in between the 'e' in welcome and the 't' in to
+console.log(javaScript[6]);     //e
---
+++ Assignments/string.practice.js
@@ -0,0 +1,12 @@
+// let originalString = "Hello";
+// let newVariable = originalString;
+// let newString = originalString.replace("e", "-");
+// console.log(newString);
+// console.log(hello);
+
+// console.log("this new string is" + newString + "it is h-llo!!" + "this is hellllllllo now" + originalString);
+
+let singleQuote = 'singleQuote';
+let doubleQuote = "doubleQuote";
+let backTicks = `backTicks`;
+console.log()
