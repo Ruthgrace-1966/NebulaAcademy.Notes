f78f2e8a039660a0c41f5b5169893488197848a9
Anitria Blue <anitria.blue@gmail.com>
2020-11-20 17:12:36 -0500

    More push practice

--- Assignments/hw10.28.20.moreFunctionReview.js
+++ Assignments/hw10.28.20.moreFunctionReview.js
@@ -3,59 +3,88 @@
 // PROBLEM 1
 // Create a function called initials that has two parameters: firstName and lastName.
 // This function will return the initials of the first and last names passed into the function.
-// function initials (firstName, lastName){
-//     return  firstName + lastName 
-// }
- 
-// // PROBLEM 2
-// // Create a function called bool that has returns the Boolean of any argument passed into it
-// function bool 
+function initials (firstName, lastName){
+    return  firstName + lastName 
+}
+console.log();
 
+
+// PROBLEM 2
+// // Create a function called bool that returns the Boolean of any argument passed into it.
+function bool (){
+    return 
+}
+console.log();
 
 
 // PROBLEM 3
 // Create a function called replaceA that takes in a string as a parameter.
 // This function will replace all a's in the string passed into the funciton with a '-'.
+function replaceA (){
+    return
+}
+ console.log();
 
- 
 
 // PROBLEM 4
 // Create a function called characterAt that takes in a string as an argument
 // This function will return the character at the 5th index of any string passed into the function
+function characterAt (){
+    return 
+}
+console.log();
 
- 
 
 // PROBLEM 5
 // Create a function called squareRoot that takes in a number as a parameter.
 // This function will return the square roof of any number that is passed in as an argument
+function squareRoot (){
+    return 
+}
+console.log();
 
- 
 
 // PROBLEM 6
 // Create a function called theLength that takes in a string as a parameter.
 // This function will return the length of any string passed into this function
+function theLength (''){
+    return 
+}
+console.log();
 
- 
 
 // PROBLEM 7
 // Create a function called lastChar that takes in a string as a parameter.
 // This function will return the last character of any string passed in as an argument
+function lastChar (''){
+    return
+}
+console.log();
 
- 
 
 // PROBLEM 8
 // Create a function called itConcats that takes in 2 parameter that are arrays.
 // This function will combine the two arrays passed into this function and return the new combined array
+function itConcats (){
+    console.log()
+    return 
+}
+ console.log();
 
- 
 
 // PROBLEM 9
 // Create a function called lastTwoEle that takes in an array as a parameter.
 // This function will return the last two elements of any array passed into this function as an argument.
+function lastTwoEle (){
+    return
+}
+console.log();
 
- 
 
 // PROBLEM 10
 // Create a function called removeFirst that takes in an array as a parameter.
 // This function removes the first elements from any array that is passed into this function as an argument.
-
+function removeFirst (){
+    return
+}
+console.log();
