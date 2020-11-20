2e39a8e8a86ababa462e355fbf1e902997efe929
Anitria Blue <anitria.blue@gmail.com>
2020-11-20 12:58:05 -0500

    Homework Assignments added to My Notes VS Code

--- Assignments/hw10.28.20.functionReview.js
+++ Assignments/hw10.28.20.functionReview.js
@@ -2,67 +2,60 @@
 
 // PROBLEM 1
 // Create a function called initials that has two parameters: firstName and lastName.
-
-// This function will return the initials of the first and last names passed into the function
-
-
+// This function will return the initials of the first and last names passed into the function.
+function initials (firstName, lastName){
+    return  firstName + lastName 
+}
  
 // PROBLEM 2
 // Create a function called bool that has returns the Boolean of any argument passed into it
+function bool 
 
 
 
 // PROBLEM 3
 // Create a function called replaceA that takes in a string as a parameter.
-
-// This function will replace all a's in the string passed into the funciton with a '-'
+// This function will replace all a's in the string passed into the funciton with a '-'.
 
  
 
 // PROBLEM 4
 // Create a function called characterAt that takes in a string as an argument
-
 // This function will return the character at the 5th index of any string passed into the function
 
  
 
 // PROBLEM 5
 // Create a function called squareRoot that takes in a number as a parameter.
-
 // This function will return the square roof of any number that is passed in as an argument
 
  
 
 // PROBLEM 6
 // Create a function called theLength that takes in a string as a parameter.
-
 // This function will return the length of any string passed into this function
 
  
 
 // PROBLEM 7
 // Create a function called lastChar that takes in a string as a parameter.
-
 // This function will return the last character of any string passed in as an argument
 
  
 
 // PROBLEM 8
 // Create a function called itConcats that takes in 2 parameter that are arrays.
-
 // This function will combine the two arrays passed into this function and return the new combined array
 
  
 
 // PROBLEM 9
 // Create a function called lastTwoEle that takes in an array as a parameter.
-
 // This function will return the last two elements of any array passed into this function as an argument.
 
  
 
 // PROBLEM 10
 // Create a function called removeFirst that takes in an array as a parameter.
-
 // This function removes the first elements from any array that is passed into this function as an argument.
 
---
+++ Assignments/hw11.11.20.codeWars.js
@@ -0,0 +1,36 @@
+// 11.11.20 CodeWars Problems
+// Due November 14, 2020 8:30 AM
+// Instructions
+// Please solve the CodeWars problems below, after you complete each problem, copy and paste your solution into VSCode, push it up to GitHub and submit your link! 
+// Add comments for each solution specifying the name of the problem to keep your assignment organized. 
+
+// https://www.codewars.com/kata/515e271a311df0350d00000f
+
+ 
+
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038
+
+ 
+
+// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+
+ 
+
+// https://www.codewars.com/kata/55a70521798b14d4750000a4
+
+ 
+
+// https://www.codewars.com/kata/571d42206414b103dc0006a1/train/javascript
+
+ 
+
+// https://www.codewars.com/kata/57d814e4950d8489720008db
+
+ 
+
+// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
+
+
+
+
+// My work
